0bc038c3641452c954619b2ed825b756eef7d6b2
Don Grote <don.grote@gmail.com>
2025-03-14 11:37:38 -0400

    Kill ffmpeg child processes when receiving SIGINT,SIGHUP, or SIGTERM (#8)
    Resolves #2

--- src/compressor.rs
+++ src/compressor.rs
@@ -1,18 +1,36 @@
+use std::cell::RefCell;
+use std::sync::mpsc;
 use std::path::PathBuf;
+use std::rc::Rc;
+use std::result::Result;
 use crate::ffmpeg::compressor::{CompressorOptions, FFmpegCompressor};
 use crate::error::{CompressorError, InputParseError};
 use crate::ffmpeg::parameter_factories::av1::Av1ParameterFactory;
 use crate::ffmpeg::parameter_factories::hevc::HevcParameterFactory;
 use crate::ffmpeg::parameter_factories::ParameterFactory;
 
-pub fn compress_file(input: &PathBuf, _output: &PathBuf, options: &CompressorOptions) -> std::result::Result<(), CompressorError> {
-    if let Ok(parameters) = create_parameter_factory(input, options) {
-        let compressor = FFmpegCompressor::new();
-        let output = generate_output_filename(&input, &options.codec);
-        compressor.compress(input, &output, &parameters)?;
-        Ok(())
-    } else {
-        Err(CompressorError::for_file(input, &format!("Unable to create {} compressor.", options.codec)))
+pub struct Compressor {
+    options: CompressorOptions,
+    events: Rc<RefCell<mpsc::Receiver<bool>>>,
+}
+
+impl Compressor {
+    pub fn new(options: CompressorOptions, events: Rc<RefCell<mpsc::Receiver<bool>>>) -> Self {
+        Compressor {
+            events,
+            options,
+        }
+    }
+
+    pub fn compress_file(&self, input: &PathBuf, _output: &PathBuf) -> Result<(), CompressorError> {
+        if let Ok(parameters) = create_parameter_factory(input, &self.options) {
+            let compressor = FFmpegCompressor::new(Rc::clone(&self.events));
+            let output = generate_output_filename(&input, &self.options.codec);
+            compressor.compress(input, &output, &parameters)?;
+            Ok(())
+        } else {
+            Err(CompressorError::for_file(input, &format!("Unable to create {} compressor.", self.options.codec)))
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ pub mod error;
 pub mod fstools;
 pub mod ffmpeg;
 
+use std::cell::RefCell;
 use std::path::PathBuf;
 use std::process::ExitCode;
+use std::rc::Rc;
+use std::sync::mpsc;
+use std::thread;
 
 use ffmpeg::compressor::CompressorOptions;
 use rustop::opts;
 use fstools::{classify_file, DirEntryCategory};
-use compressor::compress_file;
+use compressor::Compressor;
+use signal_hook::{consts::{SIGINT, SIGHUP, SIGTERM}, iterator::Signals};
 
 fn main() -> ExitCode {
     let (args, _rest) = opts! {
@@ -31,11 +36,39 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
+    let (tx, rx) = mpsc::channel::<bool>();
+    let rx = Rc::new(RefCell::new(rx));
+    let compressor = Compressor::new(CompressorOptions {
+        dry_run: args.dry_run,
+        fast: args.fast,
+        overwrite: false,
+        codec: args.codec.to_lowercase(),
+    }, Rc::clone(&rx));
+
+    thread::spawn(move || {
+        if let Ok(mut signals) = Signals::new(&[SIGINT, SIGHUP, SIGTERM]) {
+            println!("Listening for SIGINT, SIGHUP, SIGTERM");
+            for sig in signals.forever() {
+                match sig {
+                    SIGINT => println!("Caught SIGINT; signalling Compressor to stop."),
+                    SIGHUP => println!("Caught SIGHUP; signalling Compressor to stop."),
+                    SIGTERM => println!("Caught SIGTERM; signalling Compressor to stop."),
+                    _ => continue,
+                };
+
+                let _ = tx.send(true);
+                break;
+            }
+        } else {
+            println!("Error registering signal handler; Ctrl-C will not save you.");
+        }
+    });
+
     // if outfile is None, use infile path with replaced extension
     // if stdout is a tty, ask for an output filename with a suggested default
     // if infile is a directory, operate on its contents
     // if infile is a file, only operate on it
-    match classify_file(&PathBuf::from(&args.infile)) {
+    let exit_code = match classify_file(&PathBuf::from(&args.infile)) {
         DirEntryCategory::Unknown => {
             println!("Unable to classify {:?}.", args.infile);
             ExitCode::FAILURE
@@ -52,12 +85,7 @@ fn main() -> ExitCode {
             println!("{:?} is a directory.", args.infile);
             ExitCode::FAILURE
         },
-        DirEntryCategory::RegularFile => match compress_file(&PathBuf::from(&args.infile), &PathBuf::from(""), &CompressorOptions {
-            dry_run: args.dry_run,
-            fast: args.fast,
-            overwrite: false,
-            codec: args.codec.to_lowercase(),
-        }) {
+        DirEntryCategory::RegularFile => match compressor.compress_file(&PathBuf::from(&args.infile), &PathBuf::from("")) {
             Ok(_) => {
                 println!("Success! ^__^");
                 ExitCode::SUCCESS
@@ -67,5 +95,7 @@ fn main() -> ExitCode {
                 ExitCode::FAILURE
             },
         },
-    }
+    };
+
+    exit_code
 }
